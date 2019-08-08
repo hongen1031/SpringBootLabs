d586b7d2711e7c5e7a263e3ce5187fe7fa181f1b
hongen <hongenjob@126.com>
2019-08-08 13:48:27 +0800

    SpringBootLabs空项目首次提交

---
+++ SpringBootLabs/src/main/java/com/hongen/springboot/SpringBootLabsApplication.java
@@ -0,0 +1,13 @@
+package com.hongen.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLabsApplication.class, args);
+	}
+
+}
---
+++ SpringBootLabs/src/test/java/com/hongen/springboot/SpringBootLabsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hongen.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLabsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
